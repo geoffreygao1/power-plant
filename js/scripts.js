604e683a69c7dcf31c6a2e50705de32fad412bdc
Geoffrey Gao <geoffreygao1@gmail.com>
2023-05-09 12:49:33 +0900

    leaving off
    Co-authored by: Noah Cowan <nsncowan@gmail.com>
    Co-authored-by: Chloe O'Neil <chloesporri@yahoo.com>

--- js/scripts.js
+++ js/scripts.js
@@ -62,3 +62,30 @@ const giveLight = changeState("light");
 const blueFood = changeState("soil")(5)
 const greenFood = changeState("soil")(10)
 const yuckyFood = changeState("soil")(-5)
+
+
+const storeState = () => {
+  let currentState = {};
+  return (stateChangeFunction) => {
+    const newState = stateChangeFunction(currentState);
+    currentState = { ...newState };
+    return newState;
+  }
+}
+
+/* function storeState() {
+  let currentState = {};
+  return function(stateChangeFunction) {
+  const newState = stateChangeFunction(currentState);
+  currentState = { ...newState };
+  return newState;
+  }
+  } */
+
+const stateControl = storeState();
+
+// const fedPlant = stateControl(blueFood);
+// // {soil: 5}
+// const plantFedAgain = stateControl(greenFood);
+// // {soil:15 }
+
